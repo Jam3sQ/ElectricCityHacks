36d2a807f8f2f76cfab5e9c30beef88256dd47ff
Xuebs <tianxuebin@hotmail.com>
2016-11-05 05:31:17 -0400

    fixed variable at wrong location

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ function init()
 {
     window.requestAnimationFrame(draw);
 
+    var rand_position_x = function (width){
+      return Math.floor(Math.random() * (width + 1));
+    };
+            
+    var ball_position_x = 10; //position of the ball in x axis
+    var ball_weight = 0;      //weight of the ball
+    var seesaw_angle = 0;
+    var weight_position = 0;  //position of the user movable weight
+            
+    var left_total_weight = 0;  //keep track of total weight on the left side of the seesaw
+    var right_total_weight = 0; //keep track of the total weight on the right side of the seesaw
+
 }
+
 
 function draw() {
 
@@ -12,22 +25,6 @@ function draw() {
 
     ctx.clearRect(0,0,500,500); // clear canvas
     ctx.save();
-
-
-
-
-
-    var rand_position_x = function (width){
-      return Math.floor(Math.random() * (width + 1));
-    };
-        		
-    var ball_position_x = 10;	//position of the ball in x axis
-    var ball_weight = 0;			//weight of the ball
-    var	seesaw_angle = 0;
-    var weight_position = 0;	//position of the user movable weight
-        		
-    var left_total_weight = 0;	//keep track of total weight on the left side of the seesaw
-    var right_total_weight = 0;	//keep track of the total weight on the right side of the seesaw
 
 
     ctx.fillStyle = "rgb(200,0,0)";
@@ -57,5 +54,5 @@ function draw() {
 }
 
 init();
-setTimeout(draw(), 1000)
+setInterval(draw(), 1000)
 
