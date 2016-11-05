f11f77d0dc7941eade4e327389b5cb730703b9e8
Xuebs <tianxuebin@hotmail.com>
2016-11-05 04:46:14 -0400

    adjusted canvas frame size, added 2 test rectangles into script to test

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 
- var background = document.getElementbyTagName('body');
-        background.style.backgroundColor = "black";
+var background = document.getElementbyTagName('body');
+    background.style.backgroundColor = "black";
         		
-        var rand_position_x = function (width){
-        return Math.floor(Math.random() * (width + 1));
-        };
+var rand_position_x = function (width){
+    return Math.floor(Math.random() * (width + 1));
+    };
         		
-        		var ball_position_x = 0;	//position of the ball in x axis
-        		var ball_weight = 0;			//weight of the ball
-        		var	seesaw_angle = 0;
-        		var weight_position = 0;	//position of the user movable weight
+var ball_position_x = 0;	//position of the ball in x axis
+var ball_weight = 0;			//weight of the ball
+var	seesaw_angle = 0;
+var weight_position = 0;	//position of the user movable weight
         		
-        		var left_total_weight = 0;	//keep track of total weight on the left side of the seesaw
-        		var right_total_weight = 0;	//keep track of the total weight on the right side of the seesaw
+var left_total_weight = 0;	//keep track of total weight on the left side of the seesaw
+var right_total_weight = 0;	//keep track of the total weight on the right side of the seesaw
+
+
+ctx.fillStyle = "rgb(200,0,0)";
+        ctx.fillRect (10, 10, 50, 50);
+
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        ctx.fillRect (30, 30, 50, 50);
             
         
         /*
