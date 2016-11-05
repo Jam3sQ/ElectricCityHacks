b1c7492c7830ceacf255c475cbbae03f90f355c3
Xuebs <tianxuebin@hotmail.com>
2016-11-05 05:22:11 -0400

    tried moving rectangle using ball_position_x variable

--- index.js
+++ index.js
@@ -31,11 +31,11 @@ function draw() {
 
 
     ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect (10, 10, 50, 50);
-    ctx.fillStyle = "rgb(0, 0, 200)";
-    ctx.fillRect (30, 30, 50, 50);
+    ctx.fillRect (ball_position_x, ball_position_x, ball_position_x, ball_position_x);
+
+    ball_position_x++;
             
-    ctx.translate(150,150);
+    
             
             /*
      						var Ball = function (rand_position_x]) {
